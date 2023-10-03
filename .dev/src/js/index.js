3991cbbb9072c7708fca9698ccadef6b63c44234
matheus <matheus@fermen.to>
2023-10-03 19:32:22 -0300

    build e ajustes

--- .dev/src/js/index.js
+++ .dev/src/js/index.js
@@ -4,4 +4,13 @@ $(".hero__slider").slick({
 	slidesToScroll: 1,
 	prevArrow: $(".prev-hero"),
 	nextArrow: $(".next-hero"),
+	responsive: [
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			},
+		},
+	],
 })
